03951ce08d57e280759ef5f8b48028d31cd60df1
hyerin1228 <hyerin1228@gmail.com>
2024-02-14 15:41:44 +0900

    first commit

---
+++ screens/MainScreen.js
@@ -0,0 +1,17 @@
+//rafce
+// rnfes
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainScreen = () => {
+  return (
+    <View>
+      <Text>MainScreen</Text>
+    </View>
+  )
+}
+
+export default MainScreen
+
+const styles = StyleSheet.create({})
