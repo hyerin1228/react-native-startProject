75a5bcf1172176c1415745858a015fa9d7530566
hyerin1228 <hyerin1228@gmail.com>
2024-02-19 18:50:14 +0900

    #5. React Native에서 SVG 사용하고, Todo List 의 스타일링 변경

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,4 +1,7 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
+import CheckboxUnchecked from '../assets/checkbox-unchecked.svg'
+import CheckboxChecked from '../assets/checkbox-checked.svg'
+import DeleteIcon from '../assets/delete.svg'
 import React from 'react'
 
 const TodoItem = () => {
@@ -8,7 +11,8 @@ const TodoItem = () => {
             hitSlop={10}
             style={styles.itemTextChecked}
         >
-
+            <CheckboxUnchecked />
+            <CheckboxChecked style={styles.itemCheckboxCheckedIcon} />
         </Pressable>
         <Text 
             style={[styles.itemText, styles.itemTextChecked]}
@@ -22,7 +26,7 @@ const TodoItem = () => {
             ]}
             hitSlop={10}
         >
-
+            <DeleteIcon />
         </Pressable>
     </View>
   )
@@ -30,4 +34,47 @@ const TodoItem = () => {
 
 export default TodoItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    itemContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 10,
+        paddingBottom: 15,
+        paddingHorizontal: 15,
+        backgroundColor: '#f7f8fa',
+    },
+    itemCheckbox: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 20,
+        height: 20,
+        marginRight: 13,
+        borderRadius: 6,
+    },
+    itemCheckboxCheckedIcon: {
+        shadowColor: '#000000',
+        shadowOpacity: 0.14,
+        shadowRadius: 8,
+        shadowOffset: {
+            width: 0,
+            height: 4
+        }
+    },
+    itemText: {
+        marginRight: 'auto',
+        paddingRight: 25,
+        fontSize: 15,
+        lineHeight: 20,
+        color: '#737373'
+    },
+    itemTextChecked: {
+        opacity: 0.3,
+        textDecorationLine: 'line-through'
+    },
+    deleteButton: {
+        opacity: 0.8,
+    },
+    deleteButtonDone: {
+        opacity: 0.3
+    }
+});
