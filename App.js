b0f4a6968d8acc26dfd58b2d493bb64de2ff36b6
hyerin1228 <hyerin1228@gmail.com>
2024-02-28 14:25:18 +0900

    #7. Login Screen 생성 및 스타일링 적용

--- App.js
+++ App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React from "react";
 
-import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux';
+import { StyleSheet } from "react-native";
+import { Provider } from "react-redux";
 
-import MainScreen from './screens/MainScreen';
-import { store } from './redux/store';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { store } from "./redux/store";
+import LoginScreen from "./screens/LoginScreen";
+import MainScreen from "./screens/MainScreen";
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
-      <MainScreen />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Login"
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Main"
+            component={MainScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
-}
+};
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
